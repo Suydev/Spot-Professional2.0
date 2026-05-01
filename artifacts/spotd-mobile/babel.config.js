eefd079f541c55ad82ac3a547f0262f0db66c495
Suydev <elixir.suyashprabhu@gmail.com>
2026-05-01 18:59:12 +0530

    fix: use require.resolve for babel-preset-expo in pnpm monorepo

--- artifacts/spotd-mobile/babel.config.js
+++ artifacts/spotd-mobile/babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    api.cache(true);
+    return {
+      presets: [
+        [
+          require.resolve("babel-preset-expo"),
+          { unstable_transformImportMeta: true },
+        ],
+      ],
+    };
   };
-};
+  
